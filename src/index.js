673d8c24e68e6009e2faa9181ab3c0e2b08df31a
alexander <alexander@plekhanov.net>
2019-06-25 18:35:10 +0300

    Sentry implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import App from './app';
+
+Sentry.init({dsn: "https://21e4194b435946e0b2e20444d6948d25@sentry.dev.happytravel.com/4"});
 
 ReactDOM.render(<App />, document.getElementById('app'));
