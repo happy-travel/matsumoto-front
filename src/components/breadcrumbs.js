b6029bf0e5327076b08bad0e7d0b7ab5045dac95
alexander <alexander@plekhanov.net>
2019-08-18 00:55:40 +0300

    small revert

--- src/components/breadcrumbs.js
+++ src/components/breadcrumbs.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
-const Breadcrumbs = ({ items = [], noBackButton }) => (
-    <div class="breadcrumbs">
-        { items.map((item, index) => (
-            <React.Fragment>
-                { item.link ?
+const Breadcrumbs = ({ items = [], noBackButton }) => {
+    var { t } = useTranslation();
 
-                    <Link to={item.link}>
-                        {item.text}
-                    </Link> :
+    return (
+        <div class="breadcrumbs">
+            { items.map((item, index) => (
+                <React.Fragment>
+                    { item.link ?
+                        <Link to={item.link}>
+                            {item.text}
+                        </Link> :
 
-                    item.text
-                }
-                { index+1 < items.length ? <span class="small-arrow-right" /> : ' '}
-                {' '}
-            </React.Fragment>
-        )) }
-        { !noBackButton && <div class="back-button"><span class="small-arrow-left" /> {t('Back')}</div>}
-    </div>
-);
+                        item.text
+                    }
+                    { index+1 < items.length ? <span class="small-arrow-right" /> : ' '}
+                    {' '}
+                </React.Fragment>
+            )) }
+            { !noBackButton && <div class="back-button"><span class="small-arrow-left" /> {t('Back')}</div>}
+        </div>
+    );
+};
 
 export default Breadcrumbs;
