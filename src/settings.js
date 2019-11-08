6553a6ff524ffc8570c879135a309caedce5aae8
alexander <alexander@plekhanov.net>
2019-11-08 21:40:15 +0700

    Config env code style improvements.

--- src/settings.js
+++ src/settings.js
@@ -1,16 +1,22 @@
+const dev = {
+    EDO_URL      : "https://edo-api.dev.happytravel.com/",
+    IDENTITY_URL : "https://identity.dev.happytravel.com/",
+    SENTRY_DSN   : "https://21e4194b435946e0b2e20444d6948d25@sentry.dev.happytravel.com/4"
+};
+
 let settings = {
     default_culture       : "en",
 
-    edo_url               : process.env.EDO_URL || "https://edo-api.dev.happytravel.com/",
+    edo_url               : process.env.EDO_URL || dev.EDO_URL,
     edo_v1                : "/api/1.0",
 
-    identity_url          : process.env.IDENTITY_URL || "https://identity.dev.happytravel.com/",
+    identity_url          : process.env.IDENTITY_URL || dev.IDENTITY_URL,
 
     auth_callback_host    : window.location.origin,
     payment_callback_host : window.location.origin,
     payment_any_cb_host   : window.location.origin,
 
-    sentryDSN             : process.env.SENTRY_DSN || "https://21e4194b435946e0b2e20444d6948d25@sentry.dev.happytravel.com/4"
+    sentry_dsn            : process.env.SENTRY_DSN || dev.SENTRY_DSN
 };
 
 if ("localhost" == window.location.hostname)
