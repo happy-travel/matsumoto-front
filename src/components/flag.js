859a510682aff2f95b462f58ea5a75c1ad8210db
alexander <alexander@plekhanov.net>
2019-11-26 23:56:46 +0700

    NIJO-78: Flag refactoring (minified x0.1)

--- src/components/flag.js
+++ src/components/flag.js
@@ -1,11 +1,8 @@
 import React from "react";
-import FlagIconFactory from "react-flag-icon-css";
-
-const FlagIcon = FlagIconFactory(React, { useCssModules: false });
 
 const Flag = ({ code }) => (
     <span class="flag">
-        <FlagIcon code={ code } />
+        <span class={"fp " + code.toLowerCase()} />
     </span>
 );
 
