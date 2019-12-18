53a0e09746d89334850cb5297c9d272dfe22ccbb
Vladimir Yamshikov <qpeela@gmail.com>
2019-12-18 10:22:46 +0500

    NIJO-75 fix translations contact and about pages

--- src/pages/common/en/contact.js
+++ src/pages/common/en/contact.js
@@ -1,38 +1,36 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
-class ContactUsPage extends React.Component {
-    render() {
-        return (
-            <div class="confirmation block document">
-                <section class="double-sections">
-                    <div class="right-section">
-                        <section>
-                            Our Customer Service team is available 24 hours a day, seven days a week.<br/>
-                            <br/>
-                            Email <a href="mailto:info@happytravel.com" class="link">info@happytravel.com</a><br/>
-                            Call +971 4 294 000 7 (UAE)<br/>
-                            <br/>
-                            Please note, all calls may be recorded for training purposes.<br/>
-                            <br/>
-                            <br/>
-                            <br/>
-                            Our Office Address<br/>
-                            <br/>
-                            HappyTravelDotCom Travel and Tourism LLC<br/>
-                            <br/>
-                            B106, Saraya Avenue building<br/>
-                            <br/>
-                            Garhoud, Deira<br/>
-                            <br/>
-                            P.O. Box 36366<br/>
-                            <br/>
-                            Dubai, United Arab Emirates<br/>
-                        </section>
-                    </div>
-                </section>
-            </div>
-        );
-    }
-}
-
-export default ContactUsPage;
+export default function () {
+    const { t } = useTranslation();
+    return (
+        <div class="confirmation block document">
+            <section class="double-sections">
+                <div class="right-section">
+                    <section>
+                        Our Customer Service team is available 24 hours a day, seven days a week.<br/>
+                        <br/>
+                        Email <a href="mailto:info@happytravel.com" class="link">info@happytravel.com</a><br/>
+                        Call +971 4 294 000 7 (UAE)<br/>
+                        <br/>
+                        Please note, all calls may be recorded for training purposes.<br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        Our Office Address<br/>
+                        <br/>
+                        {t('footer_address_line_1')}<br/>
+                        <br/>
+                        {t('footer_address_line_2')}<br/>
+                        <br/>
+                        {t('footer_address_line_3')}<br/>
+                        <br/>
+                        {t('footer_address_line_4')}<br/>
+                        <br/>
+                        {t('footer_address_line_5')}<br/>
+                    </section>
+                </div>
+            </section>
+        </div>
+    );
+};
