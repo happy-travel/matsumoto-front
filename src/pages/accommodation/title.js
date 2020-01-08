7a32ff52dddc2de99f02687edbfeb373161dc70a
alexander <alexander@plekhanov.net>
2020-01-08 19:44:31 +0300

    Removed prices from the title page.

--- src/pages/accommodation/title.js
+++ src/pages/accommodation/title.js
@@ -22,14 +22,12 @@ render () {
                         city: 'PARIS, FRANCE',
                         flag: 'fr',
                         propertiesCount: '161',
-                        minPrice: '29.98',
                         image: '/images/hotels/france.png'
                     },
                     {
                         city: 'LONDON, ENGLAND',
                         flag: 'gb',
                         propertiesCount: '334',
-                        minPrice: '44.34',
                         image: '/images/hotels/london.png'
                     }
                 ]} />
@@ -38,21 +36,18 @@ render () {
                         city: 'ROME, ITALY',
                         flag: 'it',
                         propertiesCount: '65',
-                        minPrice: '34.11',
                         image: '/images/hotels/rome.png'
                     },
                     {
                         city: 'BARCELONA, SPAIN',
                         flag: 'es',
                         propertiesCount: '223',
-                        minPrice: '32.32',
                         image: '/images/hotels/barcelona.png'
                     },
                     {
                         city: 'DORTMUND, GERMANY',
                         flag: 'fr',
                         propertiesCount: '11',
-                        minPrice: '23.17',
                         image: '/images/hotels/dortmund.png'
                     }
                 ]} />
@@ -61,25 +56,21 @@ render () {
                     {
                         title: 'EMERALD PALACE KEMPINSKI DUBAI, DUBAI',
                         flag: 'ae',
-                        exclusive: true,
                         image: '/images/hotels/emeraldplace.png'
                     },
                     {
                         title: 'HILTON BAKU, BAKU',
                         flag: 'az',
-                        exclusive: true,
                         image: '/images/hotels/hilton.png'
                     },
                     {
                         title: 'KEMPINSKI HOTEL MALL OF THE EMIRATES, DUBAI',
                         flag: 'ae',
-                        exclusive: true,
                         image: '/images/hotels/kempinski.png'
                     },
                     {
                         title: 'PULLMAN DUBAI CREEK CITY CENTRE HOTEL, DUBAI',
                         flag: 'ae',
-                        exclusive: true,
                         image: '/images/hotels/pullman.png'
                     }
                 ]} />
