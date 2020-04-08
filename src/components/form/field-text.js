72447b61295b97af120c2d650642611d2a369c89
Vladimir Yamshikov <qpeela@gmail.com>
2020-04-08 18:55:59 +0500

    NIJO-351 fix with clear field

--- src/components/form/field-text.js
+++ src/components/form/field-text.js
@@ -100,7 +100,7 @@ class FieldText extends React.Component {
 
     clear() {
         if (this.props.formik) {
-            this.props.formik.setFieldValue(this.props.id, '');
+            this.props.formik.setFieldValue(this.props.id, '\n');
             if (this.props.onClear)
                 this.props.onClear();
             if (this.props.Dropdown)
