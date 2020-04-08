1d2f91932e0ab25a7db7f899c5a8d1c835e12151
Alexander <alexander@plekhanov.net>
2020-04-08 17:25:23 +0300

    Letter Case in destination field front workaround removed.

--- src/stores/view-store.js
+++ src/stores/view-store.js
@@ -3,8 +3,6 @@ import { observable } from "mobx";
 import UI from "./ui-store";
 import { decorate } from "core";
 
-const capitalizeString = str => str.toLowerCase().replace(/(?:^|\s)\S/g, a => a.toUpperCase());
-
 class ViewStore {
     @observable countries = [];
     @observable destinations = [];
@@ -43,7 +41,7 @@ class ViewStore {
         if (value) {
             this.destinations = value.sort((a, b) => typesWeights[b.type?.toLowerCase()] - typesWeights[a.type?.toLowerCase()]);
             this.destinations = this.destinations.filter(item => decorate.cutFirstPart(item.value, currentValue));
-            this.destinations.forEach((item, index) => this.destinations[index].value = capitalizeString(item.value));
+            this.destinations.forEach((item, index) => this.destinations[index].value = item.value);
         }
         else
             this.destinations = [];
