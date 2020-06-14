2bc506379441ae558f7fe5cb871174314014051e
Alexander <alexander@plekhanov.net>
2020-06-15 01:05:24 +0300

    Retail site styles improvements.

--- styles/index.js
+++ styles/index.js
@@ -1,4 +1,6 @@
 import './base/reset.sass';
-import './images/flagpack/dist/flagpack.css';
+import './images/flagpack/dist/flagpack.css'; // Comment this when you need to rebuild wordpress retail site css bundle
 
 export default (r => r.keys().map(r))(require.context('./', false, /\.(sass|scss)$/));
+
+// import './retail/site.sass'; // Uncomment this when you need to rebuild wordpress retail site css bundle
