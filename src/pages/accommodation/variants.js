f4eb1eed7eacdb5fbd8cf99086a567913d0de18d
Alexander <alexander@plekhanov.net>
2020-10-08 14:48:58 +0300

    Email address of Operations team changed.

--- src/pages/accommodation/variants.js
+++ src/pages/accommodation/variants.js
@@ -122,7 +122,7 @@ class AccommodationVariantsPage extends React.Component {
                                 {t("You could reach our Operations team directly, and we pick an accommodation for you.")}
                                 <br/>
                                 <br/>
-                                {t("Email")}: <a href="mailto:info@happytravel.com" class="link">info@happytravel.com</a>
+                                {t("Email")}: <a href="mailto:reservations@happytravel.com" class="link">reservations@happytravel.com</a>
                             </div>
                         </div>
                     </div> }
