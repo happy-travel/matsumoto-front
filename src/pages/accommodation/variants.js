1b885743349e711d1534e5f3758517ea184b81f5
Alexander <alexander@plekhanov.net>
2020-10-20 22:41:45 +0300

    Minor layout hotfix.

--- src/pages/accommodation/variants.js
+++ src/pages/accommodation/variants.js
@@ -199,14 +199,14 @@ class AccommodationVariantsPage extends React.Component {
                                     <GroupRoomTypesAndCount t={t} contracts={roomContractSet.roomContracts} />
                                 </h3>
                                 <div>
-                                    {!roomContractSet.roomContracts[0]?.isDynamic === true &&
+                                    {roomContractSet.roomContracts[0]?.isDynamic === true &&
                                         <div>
                                             <strong>
                                                 {t("Dynamic offer")}
                                             </strong>
                                         </div>
                                     }
-                                    {!roomContractSet.roomContracts[0]?.isAdvancedPurchaseRate &&
+                                    {roomContractSet.roomContracts[0]?.isAdvancedPurchaseRate &&
                                         <div>
                                             <span class="restricted-rate">
                                                 {t("Restricted Rate")}
