f46844ac261f33dd121f24cee4268741e9326c89
Alexander <alexander@plekhanov.net>
2020-11-24 17:45:49 +0300

    Price js error workaround.

--- src/simple/formatters/price.js
+++ src/simple/formatters/price.js
@@ -1,10 +1,16 @@
+const ALLOWED_CURRENCIES = ["USD", "AED"];
+
 export const price = (currencyOrObject, value) => {
     if (undefined === value && currencyOrObject)
         return price(currencyOrObject.currency, currencyOrObject.finalPrice || currencyOrObject.amount);
 
+    var currency = currencyOrObject;
+    if (ALLOWED_CURRENCIES.indexOf(currency) < 0)
+        currency = ALLOWED_CURRENCIES[0];
+
     var result = (value || 0).toLocaleString(undefined, {
         style: "currency",
-        currency: currencyOrObject || "USD"
+        currency
     });
 
     if (result.substr(0,2) == "US") {
