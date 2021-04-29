0ecf2c1c8590dcf642b0d87c000c7009fd8e8d71
Alexander <alexander@plekhanov.net>
2021-04-29 09:19:25 +0300

    Locale undefined in localstorage - bugfix.

--- src/core/misc/locale.js
+++ src/core/misc/locale.js
@@ -1,16 +1,19 @@
 import { windowLocalStorage } from "core/misc/window-storage";
 import i18n from "i18next";
-import settings from "settings";
 
 const WINDOW_LOCAL_STORAGE_KEY = "locale";
+const SUPPORTED_LOCALES = ["en", "ar"];
 
 const getLocale = () => {
     if (window.__ht_locale)
         return window.__ht_locale;
 
-    const result = windowLocalStorage.get(WINDOW_LOCAL_STORAGE_KEY) || settings.default_culture;
-    window.__ht_locale = result;
-    return result;
+    let newLocale = windowLocalStorage.get(WINDOW_LOCAL_STORAGE_KEY);
+    if (!SUPPORTED_LOCALES.includes(newLocale))
+        newLocale = SUPPORTED_LOCALES[0];
+
+    window.__ht_locale = newLocale;
+    return newLocale;
 };
 
 const setLocale = (locale) => {
