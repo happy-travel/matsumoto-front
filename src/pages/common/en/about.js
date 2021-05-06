319ddf4fa9b0fe6b310170ea4bb21ffafa5a8fef
Alexander <alexander@plekhanov.net>
2021-05-07 00:41:21 +0300

    Update of About Us pages.

--- src/pages/common/en/about.js
+++ src/pages/common/en/about.js
@@ -4,19 +4,47 @@ export default () => (
     <div className="document block">
         <section>
             <h1>About Us</h1>
-            <p><b>HappyTravelDotCom</b> is a new travel wholesaler based in Dubai servicing businesses such as travel agents and tour operators across the world.</p>
-
-            <p>With our global database of contacts, we can offer a premium service with over 2000 properties, covering a range of international territories. You’ll find properties in different destinations such as Paris, Maldives, London and Dubai, all designed to offer an exquisite ground service from the moment you step off the plane.</p>
-
-            <p><i>Wherever you want to travel, we have the solution.</i></p>
-
-            <p>The robust online platform is run by a team of friendly experts, who have over 15 years’ experience in the travel and hospitality sector. Here at <b>HappyTravelDotCom</b>, we’re particularly passionate about what we do and it’s a pleasure to help you find the right service.</p>
-
-            <p>We designed <b>HappyTravelDotCom</b> with you in mind. Constructed by top IT specialists, our algorithms are able to be tailored and filtered to fit your needs. <b>HappyTravelDotCom</b> are committed to offering you the best price possible.</p>
-            <p>The <b>HappyTravelDotCom</b> website and mobile app are available in English, Arabic, and Russian.</p>
-
-            <p>For a travel professional, <b>HappyTravelDotCom</b> is the answer.<br/>
-                If you’d like to reach the team at <b>HappyTravelDotCom</b>, you can call 24/7/366. We look forward to hearing from you!</p>
+            <h1>Offering premier destinations for the world’s most discerning travellers</h1>
+            <p>Happy Travel & Tourism is a luxury b2b global travel wholesaler. Headquartered in Dubai, United Arab Emirates, Happy Travel and Tourism also has offices in Saudi Arabia and Russia and sales representation in Europe and Mexico. Upcoming plans include expansion in Europe and LATAM and further sales representations in India and China.</p>
+            <p>We believe an elite travel experience ignites all the senses from sight, sound, smell, taste, to touch. We connect your clients with the best of the destinations, offering cultural experiences, gourmet cuisine and luxury accommodation to create treasured, long-lasting memories.</p>
+            <p>We believe the client experience should be flawless and that you cannot put a price on the extraordinary experience of luxury travel. With an unending focus on this commitment, our best-in-class offerings will exceed your clients expectations.</p>
+        </section>
+        <section>
+            <h1>Who We Are</h1>
+            <p>Happy Travel & Tourism was founded by a team of dedicated and passionate professionals with decades of experience in travel and hospitality and vast professional networks.</p>
+            <h2><b>Our Founders</b></h2>
+            <p><b>Ekaterina Dolmatova - Managing Director</b><br/>
+                Ekaterina started in the UAE with a transportation business and today is managing one of the top luxury transport companies in the country.  In 2019, Ekaterina founded Happy Travel & Tourism with her husband, Dennis Dolmatov, and is involved in building the company towards the goal of being a leading global wholesaler.
+            </p>
+            <p><b>Dennis Dolmatov – Chief Business Development Officer</b><br/>
+                Dennis Dolmatov has over 20 years of experience within the hospitality and travel industry. After his first experience in retail agency management, Dennis joined a leading global wholesaler in which his long career experience spanned product development, specialised destination management and business development leadership.
+            </p>
+            <h2><b>Our Team</b></h2>
+            <p>Our management team have a combined industry experience of over 60 years with world class luxury hotels, a global leading wholesaler, leading destination management companies and multinational auditing companies.</p>
+            <p>Our young, diverse and dynamic operations team come from leading travel industry companies and bring decades of experience, unmatched passion and great energy that adds to the can-do spirit!  Language skills include English, Arabic, Russian, German, French, Hindi and Sinhala.</p>
+            <h2><b>Information Technology Team</b></h2>
+            <p>Our IT team has over 12 specialists with a wide scope of significant expertise and come from leading website portals and multinational corporations internet providers.   Each team member has an average of 10 years’ experience and have led the development of cutting-edge solutions from financial services to the space industry!</p>
+            <p><b>Kirill Taran - Chief Technology Officer</b><br/>
+                Kirill has a decade of experience leading the development of social networks and integrations of complex systems, including one of Russia’s biggest web portals and a multinational corporate internet provider.  Kirill speaks English, Russian, German and Japanese!
+            </p>
+            <p><b>Vadim Mingazhev - Lead Developer</b><br/>
+                Vadim has a decade of experience in software development and engineering including developing Enterprise Content Management systems with a leading ECM systems provider.  Vadim also worked with a leading IT services company that provided IT services to fortune 500 companies.  In his <b>spare time</b> Vadim organises and speaks at IT specialised conferences.
+            </p>
+        </section>
+        <section>
+            <h1>What We Do</h1>
+            <p>Your VIP clients expect and deserve the best experience in leading destinations from luxury accommodation to once-in-a-lifetime experiences. Our mission is to offer the best guidance and support your planning needs, combined with a highly personalised service available 24 hours a day and 7 days a week.</p>
+            <p>Our destination experts craft one-of-a-kind experiences for the most discerning travelers, starting with destination ideation, to arrivals and through to departures. Our trusted advisors are always keen to share their passion and knowledge.</p>
+        </section>
+        <section>
+            <h1>Our Unmatched Expertise</h1>
+            <p>The expertise of our team at Happy Travel & Tourism means that we provide a client experience unlike any other.</p>
+            <ul>
+                <li><b>24/7 Concierge Service:</b> We provide personalised concierge services 24 hours a day and 7 days a week. Our commitment to excellence means we offer only the best at every client touchpoint.</li>
+                <li><b>State-of-the-Art Technology:</b> We remain on the cutting-edge of technology with our easy-to-use state-of-the-art online system, offering direct connectivity to the leading global hotel chains.</li>
+                <li><b>The Best in E-Commerce:</b> Our expertise in e-commerce allows us to engage in dynamic pricing and dynamic exchange rates offering our clients the most seamless experience possible and competitive offerings.</li>
+                <li><b>Meeting Evolving Needs:</b> The luxury travel segment changes and evolves constantly. To keep pace with the current times, demands and advancements, we continuously follow luxury travel trends and emerging destinations to continuously meet the needs and enhance our services.</li>
+            </ul>
         </section>
         <section>
             <h1>Mission</h1>
